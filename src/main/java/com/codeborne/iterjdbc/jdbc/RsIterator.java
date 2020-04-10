545e402549fb4b06b5d636b1d0d202e1d56b7f3a
Ilja Tollu <im.tollu@gmail.com>
2020-04-10 11:38:46 +0300

    add RsIterator to iterate a ResultSet and provide CloseableIterator

---
+++ src/main/java/com/codeborne/iterjdbc/jdbc/RsIterator.java
@@ -0,0 +1,59 @@
+package com.codeborne.iterjdbc.jdbc;
+
+import com.codeborne.iterjdbc.CloseableIterator;
+import com.codeborne.iterjdbc.WithCloseHandlers;
+import com.codeborne.iterjdbc.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+public class RsIterator<E> extends WithCloseHandlers implements CloseableIterator<E> {
+  private final ResultSet rs;
+  private final RowMapper<E> rowMapper;
+  private Boolean hasNext;
+
+  public RsIterator(ResultSet rs, RowMapper<E> rowMapper) {
+    this.rs = rs;
+    this.rowMapper = rowMapper;
+    this.onClose(this::closeRs);
+  }
+
+  @Override
+  public boolean hasNext() {
+    if (hasNext == null) {
+      advance();
+    }
+    return hasNext;
+  }
+
+  @Override
+  public E next() {
+    if (!hasNext) {
+      throw new NoSuchElementException();
+    }
+    try {
+      E next = rowMapper.mapRow(rs);
+      advance();
+      return next;
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void advance() {
+    try {
+      hasNext = rs.next();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void closeRs() {
+    try {
+      rs.close();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ src/test/java/com/codeborne/iterjdbc/jdbc/RsIteratorTest.java
@@ -0,0 +1,48 @@
+package com.codeborne.iterjdbc.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+class RsIteratorTest {
+  ResultSet rs = mock(ResultSet.class);
+
+  @Test
+  void emptyResultSet() throws SQLException {
+    when(rs.next()).thenReturn(false);
+    var rsIterator = new RsIterator<>(rs, rs -> rs.getString("SHOULD_NOT_BE_CALLED"));
+
+    verify(rs, never()).getString(any());
+    assertThat(rsIterator.hasNext()).isFalse();
+    assertThatThrownBy(rsIterator::next).isInstanceOf(NoSuchElementException.class);
+  }
+
+  @Test
+  void iterate() throws SQLException {
+    when(rs.next()).thenReturn(true, true, true, false);
+    when(rs.getString(any())).thenReturn("A", "B", "C");
+
+    var rsIterator = new RsIterator<>(rs, rs -> rs.getString("COLUMN_NAME"));
+
+    assertThat(rsIterator).toIterable().containsExactly("A", "B", "C");
+  }
+
+  @Test
+  void close_closesResultSet() throws SQLException {
+    var rsIterator = new RsIterator<>(rs, rs -> 0);
+
+    rsIterator.close();
+
+    verify(rs).close();
+  }
+
+  @Test
+  void next() {
+  }
+}
