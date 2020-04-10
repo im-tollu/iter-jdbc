b64a3e21e7afa7a025b77d7be7300ba85861c3f1
Ilja Tollu <im.tollu@gmail.com>
2020-04-10 23:43:15 +0300

    reformat for better readability

--- src/main/java/com/codeborne/iterjdbc/jdbc/Queries.java
+++ src/main/java/com/codeborne/iterjdbc/jdbc/Queries.java
@@ -13,22 +13,14 @@ public class Queries {
     this.preparedQueries = preparedQueries;
   }
 
-  public <E> CloseableIterator<E> executeQuery(
-    String sql,
-    Map<String, Object> params,
-    RowMapper<E> rowMapper
-  ) {
+  public <E> CloseableIterator<E> executeQuery(String sql, Map<String, Object> params, RowMapper<E> rowMapper) {
     var query = preparedQueries.prepareQuery(sql, rowMapper);
     var results = query.execute(params);
     results.onClose(query::close);
     return results;
   }
 
-  public <E> E executeQueryForSingleResult(
-    String sql,
-    Map<String, Object> params,
-    RowMapper<E> rowMapper
-  ) {
+  public <E> E executeQueryForSingleResult(String sql, Map<String, Object> params, RowMapper<E> rowMapper) {
     try (var results = executeQuery(sql, params, rowMapper)) {
       return results.hasNext() ? results.next() : null;
     }
