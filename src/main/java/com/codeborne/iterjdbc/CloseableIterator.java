288bd13d301b522e4feb804c01f05eaaf26eebc8
Ilja Tollu <im.tollu@gmail.com>
2020-04-10 17:39:44 +0300

    don't throw checked exceptions from CloseableIterator.close

--- src/main/java/com/codeborne/iterjdbc/CloseableIterator.java
+++ src/main/java/com/codeborne/iterjdbc/CloseableIterator.java
@@ -3,6 +3,9 @@ package com.codeborne.iterjdbc;
 import java.util.Iterator;
 
 public interface CloseableIterator<E> extends Iterator<E>, AutoCloseable {
+  @Override
+  void close();
+
   default void onClose(Runnable closeHandler) {
     throw new UnsupportedOperationException();
   }
